b47711d15441724eea20e86167ba07c7fba2f946
hammadahmed101 <11hammadahmad22@gmail.com>
2025-12-09 16:30:27 +0000

    Khao peo

--- sadi.cpp
+++ sadi.cpp
@@ -3,5 +3,7 @@ using namespace std;
 
 int main() {
     cout << "Meri jind meri jaan, general asim munir" << endl;
+    cout << "Follow laaallipop35 on instagram" << endl;
+    cout << "Order now from Khao Peo Services" << endl;
     return 0;
 }
